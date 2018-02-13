2e65cca62c299bc79c340260de03ff79718016d4
Alex Clink <code@alexclink.com>
2018-02-13 00:50:09 -0500

    Use floats instead of ints

--- lib/timesplit/split.rb
+++ lib/timesplit/split.rb
@@ -1,14 +1,14 @@
 module Timesplit
   class Split
 
-    SECS = 60
+    SECS = 60.0
 
     def self.[](*values)
       new(*values)
     end
 
     def initialize(*times)
-      times = times.first.split(/[^\d]+/).map(&:to_i) if times.first.is_a? String
+      times = times.first.split(/[^\d]+/).map(&:to_f) if times.first.is_a? String
       multiplier = 1
       @seconds = times.reverse.reduce do |t, n|
         t += n * (multiplier *= SECS)
@@ -29,38 +29,42 @@ module Timesplit
 
     def to_s
       [
-        hours.to_s.rjust(2, '0'),
-        minutes.to_s.rjust(2, '0'),
-        seconds.to_s.rjust(2, '0'),
+        hours.to_i.to_s.rjust(2, '0'),
+        minutes.to_i.to_s.rjust(2, '0'),
+        seconds.to_i.to_s.rjust(2, '0'),
       ].join(':')
     end
 
     def to_i
-      @seconds
+      @seconds.to_i
+    end
+
+    def to_f
+      @seconds.to_f
     end
 
     def ==(other)
-      self.to_i == other.to_i
+      self.to_f == other.to_f
     end
 
     def coerce(other)
-      [Split.new(other.to_i), self]
+      [Split.new(other.to_f), self]
     end
 
     def +(other)
-      Split.new(@seconds + other.to_i)
+      Split.new(@seconds + other.to_f)
     end
 
     def -(other)
-      Split.new(@seconds - other.to_i)
+      Split.new(@seconds - other.to_f)
     end
 
     def /(other)
-      Split.new(@seconds / other.to_i)
+      Split.new(@seconds / other.to_f)
     end
 
     def *(other)
-      Split.new(@seconds * other.to_i)
+      Split.new(@seconds * other.to_f)
     end
 
     private
